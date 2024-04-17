41b687ad21e6913a335f33d0f0ff85167745d636
Andrés <95057542+andreselc@users.noreply.github.com>
2024-04-16 22:04:37 -0400

    User's model and first dto created.

---
+++ Modelos/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackendSAP.Modelos
+{
+    public class Usuarios : IdentityUser
+    {
+        //Añadir campos personalizados
+        [Required]
+        public string UserName { get; set; }
+
+    }
+
+}
+
