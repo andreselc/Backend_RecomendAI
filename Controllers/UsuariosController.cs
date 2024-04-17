d0ac4e8c92a54b45663c9cfc35fea50e2727610b
Andrés <95057542+andreselc@users.noreply.github.com>
2024-04-16 23:55:34 -0400

    Users controller implemented with new Dto.

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,6 +1,114 @@
-﻿namespace IARecommendAPI.Controllers
+﻿using AutoMapper;
+using IARecommendAPI.Modelos;
+using IARecommendAPI.Modelos.Dtos.Usuarios;
+using IARecommendAPI.Repositorios.IRepositorios;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace IARecommendAPI.Controllers
 {
-    public class UsuariosController
+    [Route("api/usuarios")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
     {
+        private readonly IUsuarioRepositorio _usRepo;
+        protected RespuestasAPI _respuestasApi;
+        private readonly IMapper _mapper;
+
+        public UsuariosController(IUsuarioRepositorio usRepo, IMapper mapper)
+        {
+            _usRepo = usRepo;
+            this._respuestasApi = new();
+            _mapper = mapper;
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetUsuarios()
+        {
+            var listaUsuarios = _usRepo.GetUsuarios();
+            var listaUsuariosDto = new List<UsuarioDto>();
+            foreach (var lista in listaUsuarios)
+            {
+                listaUsuariosDto.Add(_mapper.Map<UsuarioDto>(lista));
+            }
+            return Ok(listaUsuariosDto);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("{usuarioId}", Name = "GetUsuario")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetUsuario(string usuarioId)
+        {
+            var itemUsuario = _usRepo.GetUsuario(usuarioId);
+            if (itemUsuario == null)
+            {
+                return NotFound();
+            }
+
+            var itemUsuarioDto = _mapper.Map<UsuarioDto>(itemUsuario);
+            return Ok(itemUsuarioDto);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("registro")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Registro([FromBody] UsuarioRegistroDto usuarioRegistroDto)
+        {
+            bool validarNombreUsuarioUnico = _usRepo.IsUniqueUser(usuarioRegistroDto.Email);
+            if (!validarNombreUsuarioUnico)
+            {
+                _respuestasApi.StatusCode = HttpStatusCode.BadRequest;
+                _respuestasApi.IsSuccess = false;
+                _respuestasApi.ErrorMessages.Add("El nombre de usuario ya existe");
+                return BadRequest(_respuestasApi);
+            }
+
+            var usuario = await _usRepo.Registro(usuarioRegistroDto);
+            if (usuario == null)
+            {
+                _respuestasApi.StatusCode = HttpStatusCode.BadRequest;
+                _respuestasApi.IsSuccess = false;
+                _respuestasApi.ErrorMessages.Add("Error en el registro");
+                return BadRequest(_respuestasApi);
+            }
+
+            _respuestasApi.StatusCode = HttpStatusCode.OK;
+            _respuestasApi.IsSuccess = true;
+            return Ok(_respuestasApi);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Login([FromBody] UsuarioLoginDto usuarioLoginDto)
+        {
+            var respuestaLogin = await _usRepo.Login(usuarioLoginDto);
+
+            if (respuestaLogin.Usuario == null || string.IsNullOrEmpty(respuestaLogin.Token))
+            {
+                _respuestasApi.StatusCode = HttpStatusCode.BadRequest;
+                _respuestasApi.IsSuccess = false;
+                _respuestasApi.ErrorMessages.Add("El nombre de usuario o password son incorrectos");
+                return BadRequest(_respuestasApi);
+            }
+
+            _respuestasApi.StatusCode = HttpStatusCode.OK;
+            _respuestasApi.IsSuccess = true;
+            _respuestasApi.Result = respuestaLogin;
+            return Ok(_respuestasApi);
+        }
     }
 }
