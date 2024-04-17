5e377413eff9ed0324ba8740db45aa7b0f040dce
Andrés <95057542+andreselc@users.noreply.github.com>
2024-04-16 22:24:47 -0400

    New Mapper file created.

---
+++ Mappers/PaginaWebMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BackendSAP.Modelos;
+using IARecommendAPI.Modelos.Dtos.Usuarios;
+
+namespace IARecommendAPI.Mappers
+{
+    public class PaginaWebMapper : Profile
+    {
+        public PaginaWebMapper()
+        {
+            CreateMap<Usuarios, UsuarioDto>().ReverseMap();
+        }
+    }
+}
