5a0283bcb7245dc499bb7385cc55371ef157013a
Andrés <95057542+andreselc@users.noreply.github.com>
2024-04-26 16:28:18 -0400

    Mappers updated.

--- Controllers/LikesController.cs
+++ Controllers/LikesController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using IARecommendAPI.Modelos;
 using IARecommendAPI.Modelos.Dtos.Likes;
+using IARecommendAPI.Modelos.Dtos.Peliculas;
 using IARecommendAPI.Repositorios.IRepositorios;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,7 +34,30 @@ namespace IARecommendAPI.Controllers
             return Ok(listaLikesDto);
         }
 
+       /* [HttpPost]
+        [ProducesResponseType(201, Type = typeof(LikeDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateLike([FromBody] CrearLikeDto crearLikeDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (crearLikeDto == null)
+            {
+                return BadRequest(ModelState);
+            }
 
-        
+            var pelicula = _mapper.Map<Pelicula>(crearPeliculaDto);
+            if (!_pRepo.CrearPelicula(pelicula))
+            {
+                ModelState.AddModelError("", $"Algo salio mal guardando el registro de {pelicula.Titulo_original}");
+                return StatusCode(500, ModelState);
+            }
+            return CreatedAtRoute("GetPelicula", new { nombre = pelicula.Titulo_original }, pelicula);
+        }*/
+
     }
 }
