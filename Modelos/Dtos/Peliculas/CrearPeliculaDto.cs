88fff832c67dcb1028ea3e67c23afcbad3756edb
Alex-Altuve <altuvealex6@gmail.com>
2024-04-28 19:40:19 -0400

    Cambios realizados en pelicula, se agrego endpoitn para buscar por genero (funcional)

--- Modelos/Dtos/Peliculas/CrearPeliculaDto.cs
+++ Modelos/Dtos/Peliculas/CrearPeliculaDto.cs
@@ -6,19 +6,22 @@ namespace IARecommendAPI.Modelos.Dtos.Peliculas
     {
 
         [Required(ErrorMessage = "El titulo_original de la pelicula es obligatorio")]
-        [MaxLength(100, ErrorMessage = "El número máximo de caracteres es 100!")]
+        [MaxLength(300, ErrorMessage = "El número máximo de caracteres es 300!")]
         public string Titulo_original { get; set; }
 
         [Required(ErrorMessage = "La descripcion de la pelicula es obligatorio")]
+        [MaxLength(1000, ErrorMessage = "El número máximo de caracteres es 1000!")]
         public string Descripcion { get; set; }
 
         [Required(ErrorMessage = "La fecha de estreno de la pelicula es obligatorio")]
-        public DateTime Fecha_estreno { get; set; }
+        public DateTime? Fecha_estreno { get; set; }
 
         [Required(ErrorMessage = "El Cartel_path (Imagen) de la pelicula es obligatorio")]
+        [MaxLength(600, ErrorMessage = "El número máximo de caracteres es 600!")]
         public string Cartel_path { get; set; }
 
         [Required(ErrorMessage = "EL Genero_Pelicula es obligatorio")]
+        [MaxLength(600, ErrorMessage = "El número máximo de caracteres es 600!")]
         public string Genero_Pelicula { get; set; }
 
     }
